e88b6dc93fc5ae1192d52a8898a6bfe894d41bff
ChansaJun <Chunsa2003junpimpa@gmail.com>
2024-12-04 13:21:19 +0700

    add souvenir shop - close #6

---
+++ entity/souvenirshop.go
@@ -0,0 +1,16 @@
+package entity
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type SouvenirShop struct {
+	gorm.Model
+	GoodsName        string    `json:"goods_name"`
+	GoodsPrice       float32   `json:"goods_price"`
+	GoodsAmoung      string    `json:"goods_amoung"`
+	GoodsDescription string    `json:"goods_description"`
+	DateAdded        time.Time `json:"date_added"`
+}
